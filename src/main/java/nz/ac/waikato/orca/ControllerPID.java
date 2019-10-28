00dbb69dca7de8b89615afc861688e42411a8fce
Harry McCarthy <d4m3on@outlook.com>
2019-10-29 12:45:54 +1300

    adds code to the pid controller

--- src/main/java/nz/ac/waikato/orca/ControllerPID.java
+++ src/main/java/nz/ac/waikato/orca/ControllerPID.java
@@ -1,12 +1,46 @@
 package nz.ac.waikato.orca;
 
 public class ControllerPID extends ControllerHead {
+	public final int INTEGRALHISTORY = 30;
+	private double perror = 0;
+	private double integral = 0;
+	private double[] integralHistory = new double[INTEGRALHISTORY];
+	private int integralHistoryPlace = 0;
+	private long dt = 0;
+	private int Kp = 0;
+	private int Ki = 0;
+	private int Kd = 0;
 
 	@Override
 	public boolean evaluate(ParameterInterface<?>[] parameters, double[] measurements, Double[] setpoints) {
 		if (areAllSetpointsNull(setpoints))
 			return false;
+		for (int i = 0; i < setpoints.length; i++) {
+			Double setpoint = setpoints[i];
+			double measurement = measurements[i];
+			if (setpoint != null) {
+				double error = setpoint - measurement;
+				double integral = calculateIntegral(error, dt);
+				double derivative = (error - perror) / dt;
+				double output = (Kp * error) + (Ki * integral) + (Kd * derivative);
+				perror = error;
+				for (ParameterInterface<?> p : parameters) {
+					p.set(output);
+				}
+			}
+		}
 		return false;
+	}
+
+	private double calculateIntegral(double error, long dt) {
+		integral = integral + error * dt;
+		if (integralHistoryPlace == INTEGRALHISTORY) {
+			integralHistoryPlace = 0;
+		}
+		integral -= integralHistory[integralHistoryPlace];
+		integralHistory[integralHistoryPlace] = integral;
+		integralHistoryPlace++;
+		return integral;
 	}
 
 	@Override
