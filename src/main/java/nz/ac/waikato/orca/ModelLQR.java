6506a97186782c5be324d43a5b024fd2ab2c4023
d4m30n <w0455rk@protonmail.com>
2019-11-15 09:13:57 +1300

    Adds a model to evaluate system from any class

---
+++ src/main/java/nz/ac/waikato/orca/ModelLQR.java
@@ -0,0 +1,28 @@
+package nz.ac.waikato.orca;
+
+public class ModelLQR {
+
+  private static double[] intercept = { 3.379724, 4.107224 };
+  public static final int CPU = 0;
+  public static final int MEMORY = 1;
+
+  private ModelLQR() {
+  };
+
+  public static double encodeMeasurement(double measurement, Integer type) {
+    return Math.log(measurement) - intercept[type];
+  }
+
+  public static double encodeParameter(double parameter) {
+    return Math.log(parameter);
+  }
+
+  public static double decodeMeasurement(double measurement, Integer type) {
+    return Math.exp(measurement + intercept[type]);
+  }
+
+  public static double decodeParameter(double parameter) {
+    return Math.exp(parameter);
+  }
+
+}
